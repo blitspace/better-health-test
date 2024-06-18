2ac79906aed774a02c7f1dfb46685346cb007054
ajcapacete <ajcapacete@gmail.com>
2024-06-18 17:39:16 +0800

    [master] Setup tailwind

--- src/Frontend/src/App.js
+++ src/Frontend/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello, React!</h1>
+        <div className="tw-bg-red-900 tw-p-8">
+            <h1 className="tw-text-red-300">Hello, React</h1>
         </div>
     );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    prefix: 'tw-',
+    content: [
+        './src/**/*.{js,php}'
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
