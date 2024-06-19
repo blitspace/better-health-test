12cfab4e1ff75efb484204fa605c7406168f1ce9
ajcapacete <ajcapacete@gmail.com>
2024-06-19 10:53:29 +0800

    [feature/ui] Add debug code

--- src/Frontend/src/List.js
+++ src/Frontend/src/List.js
@@ -13,6 +13,10 @@ const List = ({ listItems =[] }) => {
                         description={item.description}
                         sku={item.sku}
                         cat={item.category}
+                        tech={item.technology}
+                        subTech={item.sub_technology}
+                        vendor={item.vendor}
+                        productType={item.product_type}
                     />
                 );
             })}
--- src/Frontend/src/ListItem.js
+++ src/Frontend/src/ListItem.js
@@ -1,18 +1,36 @@
 import React from "react";
-import List from "./List";
 
+const Tag = ({ label }) => {
+    return (<span className="bg-blue-300 ml-1 p-1 rounded-md">{label}</span>);
+};
 
-const ListItem = ({ title, description, sku, cat }) => {
+const ListItem = ({ title, description, sku, cat, tech, subTech, vendor, productType }) => {
     return (<div className="p-4 border border-solid rounded-md mb-4">
         <div className="flex flex-row justify-between">
-            <div className="font-bold">{ title }</div>
-            <div className="uppercase text-sm">{ sku }</div>
+            <div className="font-bold">{title}</div>
+            <div className="uppercase text-sm">{sku}</div>
         </div>
         <div>
-            { description }
+            {description}
         </div>
-        <div className="text-xs mt-2">
-            cat: <span className="bg-blue-300 ml-2 px-2 py-1 rounded-md">{ cat }</span>
+
+        <div className="text-xs mt-4 flex flex-row gap-4">
+            <div>DEBUG:</div>
+            <div>
+                cat: <Tag label={cat} />
+            </div>
+            <div>
+                tech: <Tag label={tech} />
+            </div>
+            <div>
+                sub-tech: <Tag label={subTech} />
+            </div>
+            <div>
+                vendor: <Tag label={vendor} />
+            </div>
+            <div>
+                product type: <Tag label={productType} />
+            </div>
         </div>
     </div>);
 };
