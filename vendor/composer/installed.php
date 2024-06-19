8a428c2655bc8add8f7520957ac5d3ba12f507ee
ajcapacete <ajcapacete@gmail.com>
2024-06-19 15:26:31 +0800

    [feature/autoloader] Commit autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'better-health-test/better-health',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '110eb0973040bd160c372612b4e2c9033d765587',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'better-health-test/better-health' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '110eb0973040bd160c372612b4e2c9033d765587',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
